b0a3f92129545113323f1b2d9ddabf3511e0bd1e
22xxx22 <hana.jo@hancom.com>
2021-03-03 14:45:03 +0900

    7 vue router

--- src/App.vue
+++ src/App.vue
@@ -3,9 +3,10 @@
     title 수정123123
     <div id="nav">
       <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/about">About</router-link> |
+      <router-link to="/xxx">xxx</router-link>
     </div>
-    <router-view/>
+    <router-view />
   </div>
 </template>
 
