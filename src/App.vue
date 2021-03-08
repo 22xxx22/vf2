a9aef8e048ce06ba7c5123f0de459cd06590c197
22xxx22 <hana.jo@hancom.com>
2021-03-08 00:37:54 -0800

    firebase 설정 완료 + realtime 읽고쓰기

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,9 @@
       <v-app-bar-nav-icon @click="drawer =! drawer" />
       <site-title :title="title"></site-title>
       <v-spacer/>
-
+      <v-btn icon @click="save"><v-icon>mdi-check</v-icon></v-btn>
+      <v-btn icon @click="read"><v-icon>mdi-numeric</v-icon></v-btn>
+      <v-btn icon @click="readOne"><v-icon>mdi-account</v-icon></v-btn>
     </v-app-bar>
     <v-navigation-drawer app v-model="drawer">
       <site-menu></site-menu>
@@ -13,7 +15,6 @@
       <router-view/>
     </v-content>
     <site-footer :footer="footer"></site-footer>
-
   </v-app>
 </template>
 
@@ -32,6 +33,28 @@ export default {
       title: 'TITLE',
       footer: 'FOOTER'
     }
+  },
+  mounted () {
+    console.log(this.$firebase)
+  },
+  methods: {
+    save () {
+      console.log('save@@@')
+      this.$firebase.database().ref().child('abcd').set({
+        title: 'abcd', text: 'zzzzzz'
+      })
+    },
+    read () {
+      this.$firebase.database().ref().child('abcd').on('value', (sn) => {
+        console.log(sn)
+        console.log(sn.val())
+      })
+    },
+    async readOne () {
+      const sn = await this.$firebase.database().ref().child('abcd').once('value')
+      console.log(sn.val())
+    }
   }
 }
+
 </script>
